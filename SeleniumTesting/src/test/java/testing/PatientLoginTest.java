56fd183593d554ed1b2034f3f53162fcb1d4ba05
HP PC <HP PC@192.168.1.86>
2021-10-25 17:19:56 +0530

    Test Patient : CheckSmaple Method added

--- SeleniumTesting/src/test/java/testing/PatientLoginTest.java
+++ SeleniumTesting/src/test/java/testing/PatientLoginTest.java
@@ -1,10 +1,34 @@
 package testing;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class PatientLoginTest {
+	
+	@BeforeMethod 
+	public void initTest() 
+	{
+			System.out.println(" Before method called ");
+	}
+
+	@AfterMethod 
+	public void destroyTest() 
+	{
+		System.out.println(" After Method called ");
+		
+	}
+	
+	@Test 
+	public void checkSample() 
+	{
+		
+		System.out.println(" Checked Smaple");
+		
+	}
+	
 	@Test
 	public void checkPatientLogin() {
-		
+		System.out.println(" Checked Patient  ");
 	}
 }
