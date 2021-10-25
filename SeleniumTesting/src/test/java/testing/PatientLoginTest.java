52509b54831c240fb334fbd667bb43990ed9974a
navneetdavang <navneetdavang@gmail.com>
2021-10-25 17:13:45 +0530

    Patient Testing init

--- SeleniumTesting/src/test/java/testing/PatientLoginTest.java
+++ SeleniumTesting/src/test/java/testing/PatientLoginTest.java
@@ -1,10 +1,16 @@
 package testing;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class PatientLoginTest {
+
+	
 	@Test
 	public void checkPatientLogin() {
 		
 	}
+
+	
 }
